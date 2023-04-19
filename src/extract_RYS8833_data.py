48680bf01f30bff7bc1d4820b46453459bbfd2fd
Victor Vinci Fantucci <victor.fantucci@grupoautcomp.com.br>
2023-04-19 17:25:52 -0300

    feat: Add the Open_NMEA_file function

--- src/extract_RYS8833_data.py
+++ src/extract_RYS8833_data.py
@@ -12,3 +12,20 @@ import src.utils
 
 script_name = os.path.basename(__file__).removesuffix(".py")
 
+def open_NMEA_file(file_path: str) -> list:
+    """
+    Open the log file containing the NMEA sentences and store them as lists inside a single list.
+
+    Args:
+        file_path (str): path to the log file containing the NMEA sentences.
+
+    Returns:
+        list: list containing lists  with all the NMEA sentences of the log file.
+    """
+    data = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            data.append(line)
+
+    return data
